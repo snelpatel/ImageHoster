6052d4f22c8f8430d59b66c91a8a5c5729f1722c
snelpatel <snelpatel@gmail.com>
2020-12-08 21:00:24 +0530

    Part B - Task 1 - Added new feature Password strength, changed redirection after successful signup (Registration).

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -40,7 +40,7 @@ public class UserController {
         model.addAttribute("User", user);
         return "users/registration";
     }
-
+    //Added Methods for password strength
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
