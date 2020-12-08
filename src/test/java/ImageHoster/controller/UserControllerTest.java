312ea222f29fc5ba42d18b435c797606c763fe49
snelpatel <snelpatel@gmail.com>
2020-12-08 22:43:16 +0530

    Resolved error in test case

--- src/test/java/ImageHoster/controller/UserControllerTest.java
+++ src/test/java/ImageHoster/controller/UserControllerTest.java
@@ -118,6 +118,7 @@ public class UserControllerTest {
 
 
     //This test checks the controller logic for user signin when user enters the username and password that has been registered and sends the POST request to the server and checks whether the logic redirects to the request handling method with request mapping of type "/images"
+    @Test
     public void signinWithCorrectCredentials() throws Exception {
         User user = new User();
         UserProfile userProfile = new UserProfile();
